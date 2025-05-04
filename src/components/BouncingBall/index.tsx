19d16777ad93176c6b6b5e985397a047618adb6e
Mathieu Joly <superpose@minimac.local>
2025-05-04 23:00:31 +0200

    add squeeze effect

--- src/components/BouncingBall/index.tsx
+++ src/components/BouncingBall/index.tsx
@@ -5,6 +5,7 @@ import { shaderMaterial } from "@react-three/drei"
 
 import fragement from "./shader/fragment.glsl"
 import vertex from "./shader/vertex.glsl"
+import { time } from "three/tsl"
 
 const BouncingBallShaderMaterial = shaderMaterial({}, vertex, fragement)
 
@@ -12,6 +13,7 @@ extend({ BouncingBallShaderMaterial })
 
 const GRAVITY = -9.8 * 2
 const JUMP_VELOCITY = 20
+const RADIUS = 1
 
 function BouncingBall({ lightPosition }: { lightPosition: THREE.Vector3 }) {
   const ballRef = useRef<THREE.Mesh>(null)
@@ -20,12 +22,12 @@ function BouncingBall({ lightPosition }: { lightPosition: THREE.Vector3 }) {
     const material = new BouncingBallShaderMaterial()
 
     material.uniforms.uLightPosition = new THREE.Uniform(lightPosition)
-    material.uniforms.uVelocity = new THREE.Uniform(0)
 
     return material
   }, [])
 
   const velocity = useRef(0)
+  const timeRatioRef = useRef(1)
 
   useFrame((_state, delta) => {
     if (!ballRef?.current) {
@@ -35,20 +37,46 @@ function BouncingBall({ lightPosition }: { lightPosition: THREE.Vector3 }) {
     const ball = ballRef.current
 
     // Update the ball's position based on the velocity and gravity
-    if (ball.position.y <= 0 && velocity.current <= 0) {
+    if (
+      ball.position.y <= -RADIUS / 2 &&
+      velocity.current <= 0 &&
+      ball.scale.y == 0.5
+    ) {
       velocity.current = JUMP_VELOCITY
     }
 
-    ball.position.y += velocity.current * delta
+    const timeRatio = timeRatioRef.current
 
-    material.uniforms.uVelocity.value = velocity.current
+    ball.position.y += velocity.current * delta * timeRatio
 
-    velocity.current += GRAVITY * delta
+    velocity.current += GRAVITY * delta * timeRatio
+
+    if (ball.scale.y < 1 && velocity.current < 0) {
+      timeRatioRef.current = Math.max(0.2, timeRatioRef.current - delta * 30)
+    } else {
+      timeRatioRef.current = Math.min(1, timeRatioRef.current + delta * 5)
+    }
+
+    if (ball.position.y < 0 && velocity.current < 0) {
+      // update y scale to make it look like a bounce
+      ball.scale.y -= delta * 10 * timeRatio
+      ball.scale.y = Math.max(ball.scale.y, 0.5)
+
+      ball.scale.x = 1 / (ball.scale.y * 1.2)
+      ball.scale.z = 1 / (ball.scale.y * 1.2)
+    } else {
+      // reset y scale
+      ball.scale.y += delta * 10 * timeRatio
+      ball.scale.y = Math.min(ball.scale.y, 1)
+
+      ball.scale.x = Math.max(1, ball.scale.x - delta * 10)
+      ball.scale.z = Math.max(1, ball.scale.z - delta * 10)
+    }
   })
 
   return (
     <mesh material={material} castShadow ref={ballRef}>
-      <sphereGeometry args={[1, 32, 32]} />
+      <sphereGeometry args={[RADIUS, 32, 32]} />
     </mesh>
   )
 }
